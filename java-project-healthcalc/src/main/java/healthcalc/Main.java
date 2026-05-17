7ace50880fb6b8a2738d320ef59af76362163c6e
soyRosana <rosanagfortes@uma.es>
2026-05-17 18:54:54 +0200

    feat: Implementar menú en main y unificar formato de las pruebas de los patrones

--- java-project-healthcalc/src/main/java/healthcalc/Main.java
+++ java-project-healthcalc/src/main/java/healthcalc/Main.java
@@ -9,26 +9,42 @@ public class Main {
     public static void main(String[] args) {
         HealthCalc calc = HealthCalcImpl.getInstance();
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Bienvenido a HealthCalc. Elige una opción (1 o 2): \n");
+        System.out.println("1. Realizar cálculos básicos\n");
+        System.out.println("2. Ver los tests de los patrones\n");
+        int n=0;
         try {
-            /* 
-            System.out.print("Enter weight (kg): ");
-            double p = sc.nextDouble();
-            System.out.print("Enter height (cm): ");
-            double a = sc.nextDouble();
-            double imc = calc.bmi(p, a);
-            System.out.println("Your BMI is: " + String.format("%.3f", imc) + " (" + calc.bmiClassification(imc) + ")");
-            System.out.print("Enter your gender (H = male, M = female): ");
-            char gender = sc.next().toUpperCase().charAt(0);
-            double ibw = calc.idealBodyWeight(a, gender);
-            System.out.println("Your IBW is: " + String.format("%.3f", ibw));
-            System.out.print("Enter your waist circumference (cm): ");
-            double waist = sc.nextDouble();
-            String wcRisk = calc.wcClassification(waist, gender);
-            System.out.println("Your Cardiovascular Risk is: " + wcRisk);
-            */
+            n=sc.nextInt();
+        }catch(Exception e){
+            System.out.println("Input no válido");
+            sc.close();
+            return;
+        }
+        try {
+            if (n == 1) {
+
+                System.out.print("Enter weight (kg): ");
+                double p = sc.nextDouble();
+                System.out.print("Enter height (cm): ");
+                double a = sc.nextDouble();
+                double imc = calc.bmi(p, a);
+                System.out.println("Your BMI is: " + String.format("%.3f", imc) + " (" + calc.bmiClassification(imc) + ")");
+                System.out.print("Enter your gender (H = male, M = female): ");
+                char gender = sc.next().toUpperCase().charAt(0);
+                double ibw = calc.idealBodyWeight(a, gender);
+                System.out.println("Your IBW is: " + String.format("%.3f", ibw));
+                System.out.print("Enter your waist circumference (cm): ");
+                double waist = sc.nextDouble();
+                String wcRisk = calc.wcClassification(waist, gender);
+                System.out.println("Your Cardiovascular Risk is: " + wcRisk);
+            }else if (n==2){
+
+            
+
             // apartado 3.a (patrón adapter)
         
-            System.out.println("PRUEBA DEL PATRÓN ADAPTER (HOSPITAL)");
+            System.out.println("\n --- PRUEBA DEL PATRÓN ADAPTER (HOSPITAL) ---\n");
             
             HealthHospital hospital = new HealthHospitalAdapter();
             
@@ -45,7 +61,7 @@ public class Main {
 
             //apartado 3b (patrón proxy)
 
-            System.out.println("PRUEBA DEL PATRÓN PROXY (ESTADÍSTICAS)");
+            System.out.println("\n --- PRUEBA DEL PATRÓN PROXY (ESTADÍSTICAS) ---\n");
             
             
             HealthStatsProxy proxy = new HealthStatsProxy();
@@ -63,23 +79,25 @@ public class Main {
             System.out.println("- IMC medio: " + String.format("%.2f", proxy.imcMedio()));
 
             //apartado 3.c (patrón decorator)
+            System.out.println("\n --- PRUEBA DEL PATRÓN DECORATOR (VERSIÓN E IDIOMA) ---\n");
             HealthHospital h= new HealthHospitalAdapter();
-            // Versión europea + mensaje en español
+
+            System.out.println("\n- Versión europea con mensaje en español:");
             HealthHospital euroEsp = new EspanolIdiomaDecorator(new EuropaZonaDecorator(h));
             euroEsp.indiceMasaCorporal(1.6f, 85000);
 
-            // Versión europea + mensaje en inglés
+            System.out.println("\n- Versión europea con mensaje en inglés:");
             HealthHospital euroEng = new InglesIdiomaDecorator(new EuropaZonaDecorator(h));
             euroEng.indiceMasaCorporal(1.6f, 85000);
 
-            // Versión americana + mensaje en español
+            System.out.println("\n- Versión americana con mensaje en español:");
             HealthHospital americaEsp = new EspanolIdiomaDecorator(new AmericaZonaDecorator(h));
             americaEsp.indiceMasaCorporal(6.3f, 192);
 
-            // Versión americana + mensaje en inglés
+            System.out.println("\n- Versión americana con mensaje en inglés:");
             HealthHospital americaEng = new InglesIdiomaDecorator(new AmericaZonaDecorator(h));
             americaEng.indiceMasaCorporal(6.3f, 192);
-
+            }
         } catch (InvalidHealthDataException e) {
             System.out.println("Error: " + e.getMessage());
         }
