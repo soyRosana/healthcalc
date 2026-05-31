11e4da463332dd6361791693be1f6118544bd7d5
soyRosana <rosanagfortes@uma.es>
2026-05-31 16:06:32 +0200

    Refactor: Añadir rangos de clasificación al enum BMICategory

--- java-project-healthcalc/src/main/java/healthcalc/model/BMICategory.java
+++ java-project-healthcalc/src/main/java/healthcalc/model/BMICategory.java
@@ -1,12 +1,27 @@
 package healthcalc.model;
 
 public enum BMICategory {
-    SEVERE_THINNESS,
-    MODERATE_THINNESS,
-    MILD_THINNESS,
-    NORMAL,
-    OVERWEIGHT,
-    OBESE_CLASS_I,
-    OBESE_CLASS_II,
-    OBESE_CLASS_III
+    SEVERE_THINNESS(0, 16),
+    MODERATE_THINNESS(16, 17),
+    MILD_THINNESS(17, 18.5f),
+    NORMAL(18.5f, 25),
+    OVERWEIGHT(25, 30),
+    OBESE_CLASS_I(30, 35),
+    OBESE_CLASS_II(35, 40),
+    OBESE_CLASS_III(40, 1000);
+    
+    private final float ini;
+    private final float fin;
+
+    BMICategory(float ini, float fin) {
+        this.ini = ini;
+        this.fin = fin;
+    }
+    public float getIni() {
+         return ini; 
+        }
+
+    public float getFin() { 
+        return fin; 
+    }
 }
