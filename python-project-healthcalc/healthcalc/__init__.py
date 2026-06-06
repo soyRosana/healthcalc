4ae4daad3ae1e1ed430b36f349bcf2f05edba3e9
soyRosana <rosanagfortes@uma.es>
2026-06-06 11:51:55 +0200

    Repo: Eliminar proyecto en Python

--- python-project-healthcalc/healthcalc/__init__.py
+++
@@ -1,5 +0,0 @@
-from .exceptions import InvalidHealthDataException
-from .health_calc import HealthCalc
-from .health_calc_impl import HealthCalcImpl
-
-__all__ = ['InvalidHealthDataException', 'HealthCalc', 'HealthCalcImpl']
--- python-project-healthcalc/healthcalc/exceptions.py
+++
@@ -1,4 +0,0 @@
-class InvalidHealthDataException(Exception):
-    """Exception raised when health data is not valid."""
-    def __init__(self, message):
-        super().__init__(message)
